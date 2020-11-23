e2e97b1e3e09ee0231227fd61a898615bf78e4a9
Aaron Padilla <samir.castill@gmail.com>
2020-11-23 21:53:20 +0200

    final
    acabado, tal vez faltan detalles de interfaz de usuario y control de errores, pero la funcionalidad esta lista

---
+++ GUI.py
@@ -0,0 +1,39 @@
+#GUI
+import tkinter as tk
+import functions
+
+window = tk.Tk()
+var1 = tk.StringVar()
+var1.set(functions.get_codigo())
+
+def anterior():
+	functions.anterior()
+	var1.set(functions.get_codigo())
+
+def siguiente():
+	functions.siguiente()
+	var1.set(functions.get_codigo())
+
+def copy_button():
+    clip = tk.Tk()
+    clip.withdraw()
+    clip.clipboard_clear()
+    clip.clipboard_append(functions.get_codigo())
+    clip.destroy()
+
+window.rowconfigure(0, minsize=50, weight=1)
+window.columnconfigure([0, 1, 2], minsize=50, weight=1)
+
+btn_decrease = tk.Button(master=window, text="anterior", command= lambda: anterior(), bg="blue", fg="green")
+btn_decrease.grid(row=0, column=0, sticky="nsew")
+
+btn_increase = tk.Button(master=window, text="siguiente", command= lambda: siguiente(), bg="blue", fg="green")
+btn_increase.grid(row=0, column=2, sticky="nsew")
+
+lbl_value = tk.Label(master=window, textvariable=var1)
+lbl_value.grid(row=0, column=1)
+
+btn_copy = tk.Button(master=window, text="copiar", command= lambda: copy_button(), bg="blue", fg="green")
+btn_copy.grid(row=0, column=3, sticky="nsew")
+
+window.mainloop()
--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import linecache
+
 def genera(list):
     lista = []
     for a in list:
@@ -7,3 +9,26 @@ def genera(list):
                 if nuevo not in list: 
                     lista.append(nuevo)
     return lista
+
+def get_apuntador():
+	f = open("apuntador.txt", "r")
+	ap = int(f.read())
+	f.close()
+	return ap
+
+def get_codigo():
+	apuntador = get_apuntador()
+	return linecache.getline('generados.txt', apuntador)
+
+def siguiente():
+	apuntador = get_apuntador()
+	f = open("apuntador.txt", "w")
+	f.write(str(apuntador+1))
+	f.close()
+
+def anterior():
+	apuntador = get_apuntador()
+	f = open("apuntador.txt", "w")
+	f.write(str(apuntador-1))
+	f.close()
+
