8b20884e0da65a5bfd0e37e444810886e03fbed7
Aaron Padilla <samir.castill@gmail.com>
2020-11-23 18:54:38 +0200

    apuntador
    implementado, falta GUI

--- generador-codigos.py
+++ generador-codigos.py
@@ -2,11 +2,11 @@ import functions
 
 f = open("ref.txt", "r")
 refcodes = f.read().split('\n')
+f.close()
 refcodes.sort()
 generados = functions.genera(refcodes)
-f2 = open("generados.txt", "w")
+f = open("generados.txt", "w")
 for i in generados: 
-	f2.write("%s\n" %i)
+	f.write("%s\n" %i)
     #print ("%s\n" %i),
-f2.close()
 f.close()
---
+++ lector.py
@@ -0,0 +1,21 @@
+import linecache
+
+#obtengo n
+f = open("apuntador.txt", "r")
+apuntador = int(f.read())
+f.close()
+
+#try:, obtengo generado(n)
+codigo = linecache.getline('generados.txt', apuntador)
+print codigo
+#exception: poner
+
+#siguiente
+f = open("apuntador.txt", "w")
+f.write(str(apuntador+1))
+f.close()
+
+#anterior
+#f = open("apuntador.txt", "w")
+#f.write(str(apuntador-1))
+#f.close()
